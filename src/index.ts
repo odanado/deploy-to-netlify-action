bd27d5d51654ba1983d08722661527c9d8f3fc70
odan <odan3240@gmail.com>
2020-02-04 21:58:53 +0900

    feat(actions): add deploy comment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { spawn } from "child_process";
 import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 type Inputs = {
   DIST_DIR: string;
@@ -14,6 +15,10 @@ function loadInputs(): Inputs {
     NETLIFY_AUTH_TOKEN: core.getInput("netlify-auth-token")
   };
 }
+
+const token = process.env.GITHUB_TOKEN!;
+
+const clinet = new github.GitHub(token);
 
 function main(): void {
   const inputs = loadInputs();
@@ -30,8 +35,21 @@ function main(): void {
     }
   );
 
-  netlify.stdout.on("data", data => {
+  netlify.stdout.on("data", (data: string) => {
     console.log(`stdout: ${data}`);
+    const matched = data.match("Live Draft URL: (.*)");
+    if (matched) {
+      const { repo, owner } = github.context.repo;
+      const number = github.context.payload.pull_request?.number;
+      if (!number) return;
+      const draftUrl = matched[1];
+      clinet.issues.createComment({
+        body: `Deployed: ${draftUrl}`,
+        repo,
+        owner,
+        number: number
+      });
+    }
   });
   netlify.stderr.on("data", data => {
     console.log(`stderr: ${data}`);
